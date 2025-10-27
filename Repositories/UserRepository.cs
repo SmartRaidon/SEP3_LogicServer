99a058d9c7cacfcc983bef6ddfcc970487a8e1bc
Daniel <danielmaks400@gmail.com>
2025-10-27 14:23:45 +0100

    small update should be working didn't check but I was fighting with this fkcing proto about 2 hours xd so I just want to save it xd

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,17 +1,29 @@
 ﻿using Entities;
 using RepositoryContracts;
-// 
+using Sep3_Proto;
+
 
 namespace Repositories;
 
 public class UserRepository : IUserRepository
 {
-    //private readonly UserRepository.UserRepositoryClient _grpcClient;
+    private readonly UserService.UserServiceClient _grpcClient;
     public async Task<User> AddAsync(User user)
     {
-        // sending 'user' to Java persistence server via gRPC
-        Console.WriteLine($"Forwarding to Java server: {user.Username}");
-        await Task.Delay(100); // simulate network delay
-        return user;
+        var request = new CreateUserRequest
+        {
+            Username = user.Username,
+            Password = user.Password
+        };
+
+        var response = await _grpcClient.CreateUserAsync(request);
+        Console.WriteLine($"Created user on Java server: {response.Username} with id {response.Id}");
+
+        return new User
+        {
+            Id = response.Id,
+            Username = response.Username,
+            Password = user.Password
+        };
     }
 }
